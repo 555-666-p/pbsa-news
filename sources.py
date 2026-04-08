b5005d223324b6d6a7dcf2ebe82b9257da2e17ce
555-666-p <pcolgan@thedotgroup.com>
2026-04-08 22:08:14 +0100

    Update sources.py

--- sources.py
+++ sources.py
@@ -11,9 +11,9 @@ SOURCES = [
 ]
 
 EMAIL_RECIPIENTS = [
-    # Add recipient email addresses here, e.g. "colleague@thedotgroup.com"
+    "pcolgan@thedotgroup.com"
 ]
 
-EMAIL_SENDER_ADDRESS = "digest@yourdomain.com"   # must be a verified sender in Brevo
+EMAIL_SENDER_ADDRESS = "pcolgan@thedotgroup.com"  # must be a verified sender in Brevo
 EMAIL_SENDER_NAME = "PBSA News Digest"
 EMAIL_SUBJECT = "PBSA News Digest -- {date}"     # {date} replaced at send time
